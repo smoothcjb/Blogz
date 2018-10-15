0898a2b8afb6eb9db4fcd8d16bc364d8a2191d31
C. Brown <cortland.brown@sbcglobal.net>
2018-10-15 09:03:49 -0500

    Completed - Required Tasks

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, session, flash
+from flask import Flask, request, redirect, render_template, session, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 import re
 
@@ -12,44 +12,54 @@ app.secret_key = 'BL123xyzOG'
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    blog_entry = db.Column(db.String(255)) 
+    message = db.Column(db.String(255)) 
 
-    def __init__(self, title, blog_entry):
+    def __init__(self, title, message):
         self.title = title
-        self.blog_entry = blog_entry
+        self.message = message
               
 
 @app.route('/newpost', methods=['POST','GET'])
 def new_post():
-    title_message='Please enter a title.'
-    body_message='Please enter a message'
+    title_error='Please enter a title.'
+    body_error='Please enter a message'
     title = ''
-    blog_entry = ''
+    message = ''
+    id = ''
     if request.method == 'POST':
         title = request.form['title']
-        blog_entry = request.form['blog_entry']
-        new_post = Blog(title,blog_entry)
-        if re.search('\w',blog_entry) and not re.search('\w',title):
-            return render_template('newpost.html', page_title="Add a Blog Entry", title_message=title_message, title=title, blog_entry=blog_entry)
+        message = request.form['message']
+        new_post = Blog(title,message)
+        if re.search('\w',message) and not re.search('\w',title):
+            return render_template('newpost.html', page_title="Add a Blog Entry", title_error=title_error, title=title, message=message)
         elif not re.search('\w',title):
-            return render_template('newpost.html', page_title="Add a Blog Entry", title_message=title_message, body_message=body_message, title=title, blog_entry=blog_entry)
-        elif not re.search('\w',blog_entry): 
-            return render_template('newpost.html', page_title="Add a Blog Entry", body_message=body_message, title=title, blog_entry=blog_entry)   
+            return render_template('newpost.html', page_title="Add a Blog Entry", title_error=title_error, body_error=body_error, title=title, message=message)
+        elif not re.search('\w',message): 
+            return render_template('newpost.html', page_title="Add a Blog Entry", body_error=body_error, title=title, message=message)   
         else:
             db.session.add(new_post)
             db.session.commit()
-        return redirect('/blog')    
+            blog_posts = Blog.query.all() 
+        for blog in blog_posts:
+            id = blog.id
+        return redirect('/blog?id={0}'.format(id))
     return render_template('newpost.html', page_title="Add a Blog Entry")
- 
+           
 
-@app.route('/blog', methods=['POST','GET'])
-def index():
-    blog_posts = Blog.query.all()
-    return render_template('blog.html', page_title='Build a Blog', blog_posts=blog_posts) 
-
-
-         
-
-
+@app.route('/blog')
+def main_blog():
+    blog_posts = ''
+    id = request.args.get('id')
+    if id != None:
+        blog_post = Blog.query.get(id)
+        title = blog_post.title
+        message = blog_post.message
+        return render_template('display.html', id=id, title=title, message=message)
+    else:
+        blog_posts = Blog.query.all()
+        return render_template('blog.html', page_title='Build a Blog', blog_posts=blog_posts)    
+        
+     
+   
 if __name__ == '__main__':
     app.run()
